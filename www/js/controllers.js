6c52dd0b914b7869424aadce0f885b2034ffcd37
stardevrk <57359125+stardevrk@users.noreply.github.com>
2019-12-13 16:43:38 -0800

    Upload Firebase Plugin Test Project

---
+++ www/js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('starter.controllers', ['ionic', 'ngCordova'])
+
+.controller('DashCtrl', function($scope, $ionicPopup) {
+  $scope.showBrowser= function() {
+    console.log("Window.VideoUpload = ", window);
+    window.VideoUpload.init();
+    window.VideoUpload.startUpload(
+      function(res) { // Upload Success
+          console.log(res);
+          var alertPopup = $ionicPopup.alert({
+            title: 'Upload Success',
+            template: res,
+          });
+          alertPopup.then(function(res) {
+            console.log('Thanks');
+          });
+      }, function(e) { // Upload failed
+        console.error(e);
+        var alertPopup = $ionicPopup.alert({
+          title: 'Upload Failed',
+          template: e,
+        });
+        alertPopup.then(function(res) {
+          console.log('Thanks');
+        });
+      }
+    );
+  };
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
